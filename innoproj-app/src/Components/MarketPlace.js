864dff353db19a1290d1e761bb4d79b8f7173e14
TharinS <tharinsandipa@gmail.com>
2023-09-03 20:42:37 +1000

    Done

--- innoproj-app/src/Components/MarketPlace.js
+++ innoproj-app/src/Components/MarketPlace.js
@@ -9,13 +9,78 @@ import {
   ThemeProvider,
   CssBaseline,
   Tooltip,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  InputAdornment,
+  IconButton,
+  Button,
+  Stack,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import darkTheme from "../Themes/DarkTheme";
+import { styled, alpha } from "@mui/material/styles";
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
 import itemData from "../Data/ImageArray";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { useNavigate, Link } from "react-router-dom";
+import SearchIcon from "@mui/icons-material/Search";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const MarketPlace = () => {
+  const StyledMenu = styled((props) => (
+    <Menu
+      elevation={0}
+      anchorOrigin={{
+        vertical: "bottom",
+        horizontal: "right",
+      }}
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    "& .MuiPaper-root": {
+      borderRadius: 6,
+      marginTop: theme.spacing(1),
+      minWidth: 180,
+      color:
+        theme.palette.mode === "light"
+          ? "rgb(55, 65, 81)"
+          : theme.palette.grey[300],
+      boxShadow:
+        "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
+      "& .MuiMenu-list": {
+        padding: "4px 0",
+      },
+      "& .MuiMenuItem-root": {
+        "& .MuiSvgIcon-root": {
+          fontSize: 18,
+          color: theme.palette.text.secondary,
+          marginRight: theme.spacing(1.5),
+        },
+        "&:active": {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            theme.palette.action.selectedOpacity
+          ),
+        },
+      },
+    },
+  }));
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   // Function to generate a fake username
   function generateFakeUsername() {
     const adjectives = [
@@ -68,11 +133,60 @@ const MarketPlace = () => {
           sx={{
             bgcolor: "#1f1f2f",
             boxShadow: "0rem 0.0rem 5em rgba(0, 0, 0, 1)",
-            pt: "10px"
+            pt: "10px",
           }}
         >
-          <Box sx={{borderBottom: "0.25rem solid indigo", pb: "0.5rem"}}>
+          <Box sx={{ borderBottom: "0.25rem solid indigo", pb: "0.5rem" }}>
             <Typography variant="h3">Marketplace</Typography>
+          </Box>
+          <Box>
+            <Stack direction={"row"} gap={2}>
+              <FormControl fullWidth sx={{ m: 1 }}>
+                <InputLabel htmlFor="outlined-adornment-amount">
+                  Search
+                </InputLabel>
+                <OutlinedInput
+                  id="outlined-adornment-amount"
+                  endAdornment={
+                    <IconButton sx={{ borderRadius: "0" }}>
+                      <SearchIcon sx={{ fill: "indigo" }} />
+                    </IconButton>
+                  }
+                  label="Amount"
+                  sx={{ borderColor: "indigo !important" }}
+                />
+              </FormControl>
+            </Stack>
+            <Box>
+              <Button
+                id="demo-customized-button"
+                aria-controls={open ? "demo-customized-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+                variant="contained"
+                disableElevation
+                onClick={handleClick}
+                endIcon={<KeyboardArrowDownIcon />}
+              >
+                Sort By
+              </Button>
+              <StyledMenu
+                id="demo-customized-menu"
+                MenuListProps={{
+                  "aria-labelledby": "demo-customized-button",
+                }}
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose} disableRipple>
+                  Ascending
+                </MenuItem>
+                <MenuItem onClick={handleClose} disableRipple>
+                  Descending
+                </MenuItem>
+              </StyledMenu>
+            </Box>
           </Box>
           <Box>
             <ImageList
